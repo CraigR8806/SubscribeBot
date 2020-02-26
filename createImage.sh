cad2a4be63b16beb1519d69a535b13a21e12a723
CraigR8806 <craig@craigramage.com>
2020-02-26 02:16:59 +0000

    Removed the Dockerfile as it will be autogenerated with the installation script.  Added the .Dockerfile as a template to be used with the installtion script

--- createImage.sh
+++ createImage.sh
@@ -8,6 +8,8 @@ readprop(){
     echo "$(grep -oP "(?<=^$1=).+" $this_dir/app.properties)"
 }
 
+cp $this_dir/.Dockerfile $this_dir/Dockerfile
+
 sed -ri "s/<adminpass>/$(readprop mongo.admin.password)/" $this_dir/Dockerfile
 sed -ri "s/<appuserpass>/$(readprop mongo.app.user.password)/" $this_dir/Dockerfile
 sed -ri "s/<bottoken>/$(readprop discord.bot.token)/" $this_dir/Dockerfile
