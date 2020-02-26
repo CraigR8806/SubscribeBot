52f32b1515599f0ee608ab87fcfdb314a0e03d1c
CraigR8806 <craig@craigramage.com>
2020-02-26 01:29:33 +0000

    Remove RHEL support and forced support for just Ubuntu 18.04 as this is the version that the dockerfile will use

--- bin/install.sh
+++ bin/install.sh
@@ -17,47 +17,24 @@ waitformongo(){
         sleep 1
     done
 }
-downloadToolsRHEL(){
-    if [ "$(sudo subscription-manager status | grep -oP "(?<=^Overall Status:).+" | tr -d " ")" != "Current" ];then
-        echo "Please register your RHEL installation before continuing with this install"
-        exit 1
-    fi
-
-    sudo cp $this_dir/../resources/mongodb-org-4.2.repo /etc/yum.repos.d
-    if [ -n "$(sudo yum install -y mongodb-org | grep 'Package mongodb-org-4.2.3-1.el8.x86_64 is already installed.')" ];then
-        read  -p "Hmm.. Looks like mongo is already installed."$'\n'"Continuing with the installtion will remove any existing mongo database"$'\n'"located at $base_dir/data."$'\n'"Are you sure you want to continue?(y/n):" res
-        [ "$res" = "n" ] && echo "OK Exiting now!" && exit 1
-    fi
-    if [ -z "$(which node)" ];then
-        sudo yum install -y gcc-c++ make
-        curl -sL https://rpm.nodesource.com/setup_13.x | sudo -E bash -
-        sudo yum install nodejs
-    fi
-}
-downloadToolsDebian(){
-    if [ -n "$(which mongo)" ] || [ -n "$(which mongod)" ];then
+downloadTools(){
+    if [ -n "$(which mongo)" ] && [ -n "$(which mongod)" ];then
         read  -p "Hmm.. Looks like mongo is already installed."$'\n'"Continuing with the installtion will remove any existing mongo database"$'\n'"located at $base_dir/data."$'\n'"Are you sure you want to continue?(y/n):" res
         [ "$res" = "n" ] && echo "OK Exiting now!" && exit 1
     else
-        sudo apt update
-        sudo apt install -y software-properties-common dirmngr
-        sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-        sudo add-apt-repository 'deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.2 main'
-        sudo apt install -y mongodb-org
+        wget -q0 - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+        echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+        sudo apt-get update
+        sudo apt-get install -y mongodb-org      
     fi
     if [ -z "$( which node)" ] || [ -z "$(which npm)" ];then
-        sudo apt update
-        sudo apt install -y nodejs npm
+        sudo apt-get update
+        sudo apt-get install -y nodejs npm
     fi
 }
 
-release=$(cat /etc/*release | grep -P "^ID_LIKE=\"?[^\"]+" | awk -F= '{print $2}' | tr -d '\"')
 
-if [ "$release" = "fedora" ];then
-    downloadToolsRHEL
-elif [ "$release" = "debian" ];then
-    downloadToolsDebian
-fi
+downloadTools
 
 mkdir -p $base_dir/data
 rm -rf $base_dir/data/*
